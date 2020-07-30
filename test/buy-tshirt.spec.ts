dc7a86f6613b3bcd56878ff0039b5aee6490c302
Manuel Quintero <36927617+manuelq12@users.noreply.github.com>
2020-07-30 16:44:28 -0500

    Adding different describes to the test (#15)
    * Adding different describes to the test
    
    * Improved descriptions

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -3,30 +3,52 @@ import { MenuContentPage, ProductListPage, ProductAddedModalPage,
   SummaryStepPage, SignInStepPage, AddressStepPage,
   ShippingStepPage, OrderSummaryPage, BankPaymentPage, PaymentStepPage} from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInStepPage: SignInStepPage = new SignInStepPage();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  it('then should be bought a t-shirt', async () => {
+describe('Given a shopping website', () => {
+  beforeAll(async() => {
     await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productListPage.addToCart();
-    await productAddedModalPage.clickModalBtn();
-    await summaryStepPage.clickCheckOut();
-    await signInStepPage.performLogin();
-    await addressStepPage.clickCheckOut();
-    await shippingStepPage.acceptTermsOfService();
-    await shippingStepPage.clickCheckOut();
-    await paymentStepPage.clickPayByBank();
-    await bankPaymentPage.clickConfirmOrder();
-    await expect(orderSummaryPage.getOrderStatus())
-    .toBe('Your order on My Store is complete.');
+  });
+  describe('When the user wants to buy a t-shirt', () => {
+    beforeAll(async() => {
+      const menuContentPage: MenuContentPage = new MenuContentPage();
+      const productListPage: ProductListPage = new ProductListPage();
+      const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+      const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+
+      await menuContentPage.goToTShirtMenu();
+      await productListPage.addToCart();
+      await productAddedModalPage.clickModalBtn();
+      await summaryStepPage.clickCheckOut();
+    });
+
+    describe('Then it must login into the website', () => {
+      beforeAll(async() => {
+        const signInStepPage: SignInStepPage = new SignInStepPage();
+        await signInStepPage.performLogin();
+      });
+
+      describe('Then it must select the default address', () => {
+        beforeAll(async() => {
+          const addressStepPage: AddressStepPage = new AddressStepPage();
+          await addressStepPage.clickCheckOut();
+        });
+
+        describe('And it must pay via bank wire', () => {
+          beforeAll(async() => {
+            const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+            const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+            const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+            await shippingStepPage.acceptTermsOfService();
+            await shippingStepPage.clickCheckOut();
+            await paymentStepPage.clickPayByBank();
+            await bankPaymentPage.clickConfirmOrder();
+          });
+          it('then the t-shirt order must be completed', async () => {
+            const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+            await expect(orderSummaryPage.getOrderStatus())
+            .toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
 });
