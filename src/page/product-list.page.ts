73c58defbe8990f35a81a7cd03d7a336e7628924
Manuel Quintero <quinteromanuel1202@gmail.com>
2020-07-31 08:37:51 -0500

    List and filter elements added

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,14 +1,22 @@
-import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
+import { $$, browser, ExpectedConditions, ElementArrayFinder } from 'protractor';
 
 export class ProductListPage {
-  private addToCartBtn: ElementFinder;
+  private products: ElementArrayFinder;
 
   constructor () {
-    this.addToCartBtn = $('a.button.ajax_add_to_cart_button.btn.btn-default');
+    this.products = $$('.product-container');
   }
 
-  public async addToCart(): Promise<void> {
-    await browser.wait(ExpectedConditions.elementToBeClickable(this.addToCartBtn), 3000);
-    await this.addToCartBtn.click();
+  public async selectProduct(productName: string): Promise<void> {
+    await browser.wait(ExpectedConditions.
+      elementToBeClickable(this.findByProduct(productName)),
+                       4000);
+    await this.findByProduct(productName).$('a.ajax_add_to_cart_button').click();
+  }
+  private findByProduct (productName:string) {
+    return this.products.filter(product => product.$('.product-name')
+    .getAttribute('title').then((result) => {
+      return result === productName;
+    })).first();
   }
 }
